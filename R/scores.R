1a445d9b2000a9c8dd1a8540b47db2dac79632f4
Robert Castelo <robert.castelo@upf.edu>
2026-06-01 00:00:36 +0200

    Fixed documentation

--- R/scores.R
+++ R/scores.R
@@ -144,7 +144,7 @@ iBIC <- function(g, dat, targets=list(0L),
 #' where observations are assumed to be independent but not identically
 #' distributed (not iid) multivariate Gaussian. This GoF score corresponds to
 #' the interventional Bayesian Gaussian equivalent (iBGe) score defined by
-#' Kuipers and Moffa (2022). By default, the arguments `targets` and
+#' Kuipers and Moffa (2025). By default, the arguments `targets` and
 #' `target.index` are set so that the calculated BIC score assumes there are
 #' no interventions in the data.
 #' 
